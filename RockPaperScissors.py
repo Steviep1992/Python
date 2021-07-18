05b2a2d1c65b988812e070f23c8a7f8d2f97effd
Steviep1992 <31046499+Steviep1992@users.noreply.github.com>
2021-07-18 15:58:58 -0400

    Add Rock Paper Scissors program
    This is a program that asks the user to write either "Rock", "Paper", or "Scissors". The program will randomly select its own selection and depending on what the player picks, as well as the computer, the outcome will show whether the player won, the computer won, or it was a tie. It will then loop through and ask the user for their input again.

---
+++ RockPaperScissors.py
@@ -0,0 +1,54 @@
+from random import randint
+
+# Set the possibilities of the computer to pick randomly
+y = ["Rock", "Paper", "Scissors"]
+
+#Setting the computer to pick a random integer from the index range of 0 - 2
+computer = y[randint(0,2)]
+
+#Set the player to the boolean of False
+player = False
+
+#While the player is False, run the below code of asking the player for an input and following the if statement depending what the player selects, and the computer randomly selects
+while player == False:
+
+    #Ask the player for the exact input of Rock, Paper, or Scissors (Case sensitive)
+    player = input("Rock, Paper, Scissors? ")
+
+    #If the player and computer has the same selection, output that the game was a tie
+    if player == computer:
+        print("TIE!\n")
+
+
+        #elif statement to run through the possibilites of IF the player selects an option, AND the computer selects the option, to display the appropriate outcome
+    elif player == "Rock":
+         if computer == "Paper":
+             print("Paper covers rock, " + "Computer wins with picking " + computer + '\n')
+
+         if computer == "Scissors":
+             print("Rock breaks Scissors, " + computer + " You Win!\n") 
+
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("Rock Breaks scissors, "  + "Computer wins with picking " + computer + '\n') 
+
+        if computer == "Paper":
+            print("Scissors cuts " + computer + " You Win!\n") 
+
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("Scissors cuts paper, "  + "Computer wins with picking " + computer + '\n')
+
+        if computer == "Rock":
+            print("Paper covers " + computer + " You Win!\n")
+
+
+    #Else nothing was selected from the above elif statement, output the below statement
+    else:
+        print("Error, check your spelling please!")
+        
+        #This loops the player once the outcome has been displayed to run the program again from the top
+    player = False
+    computer = y[randint(0,2)]
+
+
